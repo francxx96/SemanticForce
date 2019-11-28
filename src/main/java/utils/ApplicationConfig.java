034b89e100c660b4fca2e6c8ff9a2cf4ebbf3f51
Francesco Riva <friva.job@outlook.com>
2019-11-28 15:13:56 +0100

    Removed json files and moved ApplicationConfig class

--- src/main/java/utils/ApplicationConfig.java
+++ src/main/java/utils/ApplicationConfig.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package crawlingScraping;
+package utils;
 
 import java.util.Set;
 import javax.ws.rs.core.Application;
