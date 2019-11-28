b4b4ba8984f213f57316e40192f7e00ae372401d
emili <emili@172.19.204.77>
2019-11-28 08:54:18 +0100

    Stanford bug fix (last entity)

--- src/main/java/ner/NERresource.java
+++ src/main/java/ner/NERresource.java
@@ -1,66 +1,105 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ner;
 
-import java.io.IOException;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.CoreMap;
 import java.util.ArrayList;
-import java.util.HashMap;
-import javax.ws.rs.Produces;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.core.MediaType;
+import java.util.List;
+import java.util.Properties;
 
-/**
- * REST Web Service
- */
-@Path("utente")
-public class NERresource {
-    private static String documentText = "";
-    private static StanfordNLP ner = new StanfordNLP();
-    private static ArrayList<Entity> entityList = new ArrayList();
-    private static HashMap<Entity, Integer> entityFreq = new HashMap();
 
-    /**
-     * Creates a new instance of UserResource
-     */
-    public NERresource() {
+public class StanfordNLP {
+
+    private Properties props; // The Properties class represents a persistent set of properties
+    
+    public StanfordNLP() {
+        props = new Properties(); // set up pipeline properties
+        // Maps the specified key to the specified value in this dictionary
+        props.put("annotators", "tokenize, ssplit, pos, lemma, ner");  // set the list of annotators to run
     }
 
     
-    public static String getDocumentText() {
-        return documentText;
-    }
+    public ArrayList<Entity> recogniseNamedEntity(String doc) {  
+        // Stanford CoreNLP provides a set of human language technology tools
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props); // build pipeline
+        Annotation document = new Annotation(doc); // create a doc object
+        pipeline.annotate(document); // annnotate the doc
+        List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class); // get the sentences contained in an annotation
 
-    public static void setDocumentText(String documentText) {
-        NERresource.documentText = documentText;
-    }
-    
-    @GET
-    @Produces(MediaType.APPLICATION_XML)
-    public static ArrayList<Entity> getEntities() throws IOException {       
-        return entityList;
-    }
-    
-    @GET
-    @Produces(MediaType.APPLICATION_XML)
-    public static HashMap getEntities(String TextArea) throws IOException {
-        //SearchEntity currEntity;
-        Integer numCurrEntity;
-        entityList = ner.recogniseNamedEntity(TextArea);
-        entityFreq.clear();
-
-        for(Entity currEntity: entityList){
-            numCurrEntity = entityFreq.get(currEntity);
-            if(numCurrEntity == null){
-                numCurrEntity = 0;
+        boolean inEntity = false;
+        String currentEntity = "", currentEntityType = "O";
+        ArrayList<Entity> entityList = new ArrayList<>();
+        Entity entity = new Entity();
+        
+        for (CoreMap sentence : sentences) {
+            for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)) { //tokens contained by an annotation
+                String text = token.get(CoreAnnotations.TextAnnotation.class);
+                //String pos = token.get(CoreAnnotations.PartOfSpeechAnnotation.class);
+                String namedEntity = token.get(CoreAnnotations.NamedEntityTagAnnotation.class);
+                //System.out.println("Token=" + token + "*\tText=" + text + "*\tPOS=" + pos + "*\tNER=" + namedEntity);
+                
+                if (!inEntity) {
+                    if (!"O".equals(namedEntity)) { // eliminate the Other categories
+                        inEntity = true;
+                        currentEntityType = namedEntity;
+                        entity = new Entity();
+                        entity.setName(text.trim());
+                        entity.setType(namedEntity);
+                        entity.setPosition(token.beginPosition());
+                    }
+                }
+                else {
+                    if (!currentEntityType.equals(namedEntity)) { //"O".equals(namedEntity) 
+                        inEntity = false;
+                        System.out.println("Extracted " + entity.getName());
+                        entityList.add(entity);
+                        //System.out.println("entityLen="+entityLength+"\toffset="+offset+"\tpos="+(offset-entityLength));
+                    }else{
+                        entity.setName(entity.getName() + " " + text);//token.originalText();
+                    }
+                }
+                /*
+                if (!inEntity) {
+                    if (!"O".equals(namedEntity)) { // eliminate the Other categories
+                        inEntity = true;
+                        currentEntity = "";
+                        currentEntityType = namedEntity;
+                        offset = token.beginPosition();
+                    }
+                }
+                if (inEntity) {
+                    if (!currentEntityType.equals(namedEntity)) { //"O".equals(namedEntity) 
+                        inEntity = false;
+                        Entity entity = new Entity();
+                        entity.setName(currentEntity.trim()); // trim() returns a copy with leading and trailing white space removed
+                        System.out.println("Extracted " + currentEntity.trim());
+                        entity.setType(currentEntityType);
+                        entity.setPosition(offset);
+                        entityList.add(entity);
+                        //System.out.println("entityLen="+entityLength+"\toffset="+offset+"\tpos="+(offset-entityLength));
+                        if (!"O".equals(namedEntity)) { // eliminate the Other categories
+                            inEntity = true;
+                            currentEntity = "";
+                            currentEntityType = namedEntity;
+                            offset = token.beginPosition();
+                        }
+                    }else{
+                        currentEntity += " " + text;//token.originalText();
+                    }
+                }
+                */
             }
-            entityFreq.put(currEntity, numCurrEntity+1);
         }
-            
-        return entityFreq;
+        
+        if(inEntity){ // useful if the text ends without the '.'
+           entityList.add(entity); 
+        }
+        
+        NERresource.setDocumentText(doc);
+        //System.out.println(entityList.toString());    
+        return entityList;                          
     }
 
 }
