b0036ab6f9ee597ed04c808d17102d270bd90284
MicheleFinamore <35727036+MicheleFinamore@users.noreply.github.com>
2019-12-04 15:15:52 +0100

    Wikidata michele preview

---
+++ src/main/java/crawlingScraping/TestCrawler.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crawlingScraping;
+
+import edu.uci.ics.crawler4j.crawler.Page;
+import edu.uci.ics.crawler4j.crawler.WebCrawler;
+import edu.uci.ics.crawler4j.parser.HtmlParseData;
+import edu.uci.ics.crawler4j.url.WebURL;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author aless
+ */
+public class TestCrawler extends WebCrawler {
+    public static ArrayList<String> cmd = new ArrayList();
+    
+    private final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|gif|jpg|png|mp3|mp4|zip|gz))$");
+
+    /**
+     * Specify whether the given url should be crawled or not based on
+     * the crawling logic. Here URLs with extensions css, js etc will not be visited.
+     * 
+     * @param referringPage
+     * @param url
+     * @return 
+     */
+    @Override
+    public boolean shouldVisit(Page referringPage, WebURL url) {
+        System.out.println("shouldVisit: " + url.getURL().toLowerCase());
+
+        String href = url.getURL().toLowerCase();
+        boolean result = !FILTERS.matcher(href).matches();
+
+        if(result)
+            System.out.println("URL Should Visit");
+        else
+            System.out.println("URL Should not Visit");
+
+        return result;
+    }
+
+    /**
+     * This function is called when a page is fetched and ready
+     * to be processed by the program.
+     * 
+     * @param page 
+     */
+    @Override
+    public void visit(Page page) {
+        String url = page.getWebURL().getURL();
+        System.out.println("URL: " + url);
+        
+        if (page.getParseData() instanceof HtmlParseData) {
+            ArrayList<String> linki = new ArrayList();
+            HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();      
+            String text = htmlParseData.getText(); //extract text from page
+            String html = htmlParseData.getHtml(); //extract html from page
+            Set<WebURL> links = htmlParseData.getOutgoingUrls();
+
+            System.out.println("---------------------------------------------------------");
+            System.out.println("Page URL: " + url);
+            System.out.println("Text length: " + text.length());
+            System.out.println("Html length: " + html.length());
+            for(WebURL w : links){
+                System.out.println(w.toString());
+                linki.add(w.toString());
+            }
+            
+            System.out.println("Number of outgoing links: " + links.size());
+            System.out.println("---------------------------------------------------------");
+
+            //if required write content to file
+            
+            System.out.println("----------------SALVATAGGIO------------------");
+            try {
+                Savatage(linki);
+            } catch (IOException ex) {
+                Logger.getLogger(TestCrawler.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+}
+    /*faccio salvare sul file perchè non ho possibilità di passare al main pur assegnando un tipo di ritorno alla funzione
+    Nel main infatti viene invocato "TestCrawler.class" senza inizializare un oggetto del tipo TestCrawler*/
+    public void Savatage(ArrayList<String> x) throws IOException{
+        cmd.addAll(x);
+        System.out.println("--------------NUOVA LISTA--------------\n"+cmd.size());
+        System.out.println(cmd.toString());
+        FileWriter w = new FileWriter("testina.txt");
+        BufferedWriter bw = new BufferedWriter(w);
+        bw.write(cmd.toString());
+        bw.flush();
+        bw.close();
+    }
+}
---
+++ src/main/webapp/responseExtraction.jsp
@@ -0,0 +1,65 @@
+<%-- 
+    Document   : responseExtraction
+    Created on : 26-set-2018, 18.55.55
+    Author     : aless
+--%>
+
+<%@page import="crawlingScraping.Extractor,java.util.ArrayList,crawlingScraping.Article" contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Extracted Articles</title>
+        <link rel="icon" href="img\tabLogo.png" type="image/x-icon"/>
+        <style type="text/css">
+            fieldset{
+                border-style: dashed;
+                border-color: #006666;
+            }
+            .bottone{
+                background-color: #33CC66;
+                border: ridge;
+                color: white;
+                font-weight: lighter;
+                border-radius: 5px;
+                
+            }
+            .bottone:hover{
+                background-color: #336666;
+            }
+        </style>
+    </head>
+    <body style="background-color:#CCFFCC">
+        <div Style="text-align:center">
+            <h1 Style="color:#006666">Articles extracted from the URLs</h1>
+        </div>
+        <p Style="text-align:justify">
+            <%  
+                String url=request.getParameter("urle");
+                String profa = request.getParameter("Cprofa");
+                String links = Extractor.getXml(url,profa);
+                ArrayList<Article> arti = Extractor.getExtractedArticles();
+            %>
+            <span>
+                <%
+                    for(Article x : arti){
+                        %>
+                    <h2 tyle="font-family: verdana;"><%out.write("TITOLO: "+x.getTitle());%></h2>
+                    <h3 tyle="font-family: verdana;"><%out.write("URL: "+x.getUrl());%></h3>
+                    <span style="font-family: verdana; font-size: 10px; color: #000000"><%out.write("TESTO ARTICOLO: "+x.getText()+"\n");%></span>
+                <%  }
+                %>
+             </span>
+        </p>
+        <div style="text-align: center">
+            <fieldset>
+                <legend><h3 style="color:#006666">Save content in JSON</h3></legend>
+                <form method="get" action="responseJSONSavatage.jsp">
+                    <tr><td><label><h3 style="color:#006666">Filename</h3></label></td><td><input name="doca" type="text" ></td></tr>
+                    <input class="bottone" type="submit" value="GO!">
+                </form>
+            </fieldset>
+        </div>
+    </body>
+    
+</html>
