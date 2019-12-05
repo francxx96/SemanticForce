e4525f200a02745b7164ca59d53fd14f54231ca2
waelkarman <wael.karman@gmail.com>
2019-12-05 10:37:05 +0100

    ricerca multiparola

--- src/test/java/TestCase.java
+++ src/test/java/TestCase.java
@@ -19,28 +19,14 @@ public class TestCase {
     
     public static void main(String[] args)throws Exception {
 
-        JSONParser parser = new JSONParser();
-       
-        String wikidatajson = getwael(enJsonForSearch("traore"));
-        JSONObject jsonObject = (JSONObject) parser.parse(wikidatajson);
-        
-        
-        System.out.println(jsonObject);
-        System.out.println(jsonObject.get("searchinfo"));
-        System.out.println(jsonObject.get("search"));
-        JSONObject jsonObject1 = (JSONObject) parser.parse(jsonObject.get("searchinfo").toString());
-        System.out.println(jsonObject1);
-        System.out.println(jsonObject1.get("search"));
-        
-        /*
-            Michele you're a bitch!
-            let's see that : https://www.mkyong.com/java/json-simple-example-read-and-write-json/
-            Kiss my ass
-        */
+  
 
-        String res = getwael("https://query.wikidata.org/sparql?query=SELECT%20DISTINCT%20%3Fitem%20%3FitemLabel%20%3Fdesc%20%0AWHERE%20%0A%7B%0A%20%20%3Fitem%20wdt%3AP31%20wd%3AQ5%3B%20%20%20%20%20%20%20%0A%20%20%20%20%20%20%20%20schema%3Adescription%20%3Fdesc%3B%0A%20%20%20%20%20%20%20%20rdfs%3Alabel%20%3FitemLabel.%0A%20%20FILTER%28LANG%28%3FitemLabel%29%20%3D%20%22en%22%29.%0A%20%20FILTER%28LANG%28%3Fdesc%29%20%3D%20%22en%22%29.%0A%20%20FILTER%20contains%28%3FitemLabel%2C%22Albert%22%29.%0A%20%20%0A%7D%0ALIMIT%203");
+        System.out.println("START!\n");
+        
+        String res = get(enJsonForSearch("albert einstein"));
         System.out.println(res);
 
+        System.out.println("\n\nrun");
 
   }
 
